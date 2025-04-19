41ecc7f6b05acd6c93fb5c6210a8e4911196523e
kunalarya873 <kunalarya873@gmail.com>
2025-04-19 10:03:46 +0530

    Varibales assignment

---
+++ 3_variables/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name = "John Doe"
+	var age int = 30
+	fmt.Println(name)
+	fmt.Println(age)
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	// Shorthand syntax
+	name1 := "Golang"
+
+	fmt.Println(name1)
+}
